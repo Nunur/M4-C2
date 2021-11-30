76734ea7a60f665e8492e4c11a31792a7856ed54
abc <labuser@ip-172-31-17-136.ap-south-1.compute.internal>
2021-11-30 22:29:00 +0000

    Addiiton Feature added to calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,9 @@
 #Interface for taking input from user
 a=int(input("Enter first number  : "))
 b=int(input("Enter second number : "))
+
+#adding addition feature
+def addition(x,y):
+    return(x+y)
+
+print(a,"+",b,"=",addition(a,b))
